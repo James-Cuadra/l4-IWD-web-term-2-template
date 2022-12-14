52b75b821b1118ea8023856654197dfa80d18d80
James Cuadra <62761473+James-Cuadra@users.noreply.github.com>
2022-12-14 09:17:38 +0000

    more changes

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -10,7 +10,6 @@ var timedifference = currenttime.getTimezoneOffset()/60;
 
 var UKcurrenthour = currenttime.getHours() + timedifference;
 
-console.log(UKcurrenthour);
 
 // do a conditional
 
@@ -28,32 +27,26 @@ getheading.insertAdjacentHTML(
 );
 
 
-
-var findLocation = document.getElementById("switch");
-
-// create a button in code
-
-var button = document.createElement("button");
-button.setAttribute("class", "js-lightmode"); // add a class we can use to style the button in CSS
-button.setAttribute("id", "lightmode"); // add an ID to listen out for clicks on the button
-button.textContent = "Change the page"; // add the text for the button
-
-// add this created button to our HTML at the place we've saved in the 'findLocation' variable
-
-findLocation.append(button); // add the button to the HTML
-
-// point JavaScript at the button we have just added and save the button location to do things with later
-
-var listenForInteraction = document.getElementById("lightmode");
+var icon = document.getElementById("switch");
 
 // This changePage function runs whenever the button is clicked on - you can see where it's being 'called' in line 34 and 35 below.
 
 function changePage() {
   // 'toggle' means swop between adding and removing the class from the class list for the HTML element. You will be able to see this happening in the web developer tools.
   document.documentElement.classList.toggle("js-modify-page");
+  if(icon.className!="light")
+    {
+        icon.src='images/light.png';
+        icon.className="light";
+    }
+    else if(icon.className=="light")
+    {
+        icon.src='images/dark.png';
+        icon.className="dark";
+    }
 }
 
 // create a click event on our new button that runs a function whenever the button is clicked
 
-listenForInteraction.addEventListener("click", changePage);
+icon.addEventListener("click", changePage);
 
